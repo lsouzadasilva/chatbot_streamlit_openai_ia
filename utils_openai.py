d6a7e0e8f18a4257080f733c9baeb563d1b729d9
Leandro Souza <161175460+lsouzadasilva@users.noreply.github.com>
2025-03-20 15:31:40 -0300

    Update utils_openai.py

--- utils_openai.py
+++ utils_openai.py
@@ -7,11 +7,15 @@ def retorna_resposta_modelo(mensagens,
                             modelo='gpt-3.5-turbo',
                             temperatura=0,
                             stream=False):
-    client = openai.OpenAI(api_key=openai_key)
-    response = client.chat.completions.create(
+
+    # Definindo a chave da API diretamente
+    openai.api_key = openai_key
+
+    response = openai.ChatCompletion.create(
         model=modelo,
         messages=mensagens,
         temperature=temperatura,
         stream=stream
     )
+
     return response
