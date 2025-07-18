8df6d6fd1693e56b59e204fd3b663baa8401cfe3
Leandro Souza <lsouzadasilva4@gmail.com>
2025-07-18 13:51:39 -0300

    Update mychatbot.py

--- mychatbot.py
+++ mychatbot.py
@@ -130,7 +130,7 @@ def tab_configuracoes(tab):
     st.sidebar.divider()
     st.sidebar.markdown("""
     **Desenvolvido por Leandro Souza**  
-    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/leandro-souza-bi/)
+    [![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in//leandro-souza-dados/)
     [![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/lsouzadasilva/chatbot_streamlit_openai)
     """)
 
